89549f54c1e7e67b648f9d0276803dca8270f0bb
mrzhenshen <mrzhenshen@gmail.com>
2024-05-18 15:02:54 +0300

    [IVAW-34]: Make button

---
+++ src/components/Button.js
@@ -0,0 +1,65 @@
+// src/components/Button.js
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Icon from "./Icon";
+
+const Button = ({ children, onClick, variant, size, icon, iconPosition }) => {
+  const baseStyles = "rounded-md hover:duration-100 ease-in-out inline-flex items-center justify-center gap-1";
+
+  const variants = {
+    primary:
+      "bg-primary-500 text-primary-0 " +
+      "hover:bg-primary-400 " +
+      "active:bg-primary-700 ",
+    secondary:
+      "bg-primary-50 text-primary-400 " +
+      "hover:bg-body-50 " +
+      "active:bg-primary-100 ",
+    destructive:
+      "border-2 border-red-800 text-red-800 " +
+      "hover:border-red-700 hover:text-red-700 " +
+      "active:border-red-900 active:text-red-900 ",
+    link: "text-primary-400 hover:bg-body-50 active:bg-primary-100 ",
+  };
+
+  const buttonSizes = {
+    default: "py-2 px-4 text-base",
+    small: "py-1 px-3 text-sm",
+  };
+
+  const iconSizes = {
+    default: "size-6",
+    small: "size-5",
+  }
+
+  const buttonClass = classNames(baseStyles, variants[variant], buttonSizes[size]);
+  const iconClass = classNames(baseStyles, iconSizes[size])
+
+  return (
+    <button onClick={onClick} className={buttonClass}>
+      {icon && iconPosition === 'start' && <Icon name={icon} className={iconClass} />}
+      <span>{children}</span>
+      {icon && iconPosition === 'end' && <Icon name={icon} className={iconClass} />}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  variant: PropTypes.oneOf(["primary", "secondary", "destructive", "link"]),
+  size: PropTypes.oneOf(["default", "small"]),
+  icon: PropTypes.string,
+  iconPosition: PropTypes.oneOf(['start', 'end']),
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  variant: "primary",
+  size: "default",
+  icon: null,
+  iconPosition: 'start',
+};
+
+export default Button;
