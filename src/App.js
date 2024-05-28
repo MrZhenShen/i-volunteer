33d195167a8bcd8f685266da33f8b44b85bdcb7e
mrzhenshen <mrzhenshen@gmail.com>
2024-05-28 13:28:07 +0300

    implement navigation bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Volunteer from './containers/Volunteer';
-import Event from './containers/Event';
-import NavigationBar from './containers/NavigationBar';
+import Volunteer from "./containers/Volunteer";
+import Event from "./containers/Event";
+import NavigationBar from "./containers/NavigationBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<NavigationBar />}>
-          <Route path="volunteer" element={<Volunteer />} />
-          <Route path="event" element={<Event />} />
+          <Route path="/volunteers" element={<Volunteer />} />
+          <Route path="/events" element={<Event />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
