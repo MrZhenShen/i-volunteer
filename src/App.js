bae03357d543f88f33345c6b29b2cec3bbc25704
Ihor_Ostapovych <iostapovychweb@gmail.com>
2024-06-05 11:33:42 +0300

    feat: implemented base Layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Volunteer from "./containers/Volunteer";
-import Event from "./containers/Event";
-import NavigationBar from "./containers/NavigationBar";
+
+import { Layout } from "./layouts/Layout";
+
+import { MapPage } from "./pages/MapPage";
+
+function Vol() {
+  return (
+    <div>
+      <h1>Volunteer</h1>
+    </div>
+  );
+}
+
+function Ev() {
+  return (
+    <div>
+      <h1>Event</h1>
+    </div>
+  );
+}
+
+
+function Analitycs() {
+  return (
+    <div>
+      <h1>Analitycs</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NavigationBar />}>
-          <Route path="/volunteers" element={<Volunteer />} />
-          <Route path="/events" element={<Event />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/volunteers" element={<Vol />} />
+          <Route path="/events" element={<Ev />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/analytics" element={<Analitycs />} />
         </Route>
       </Routes>
+
     </BrowserRouter>
   );
 }
