db8674dd6158b440a3fb8fc0a4b68d422a6da77f
ihorhaidar <ihor.haidar.ua@gmail.com>
2024-06-10 21:04:33 +0300

    Analytics page layout

--- src/pages/AnalyticsPage.js
+++ src/pages/AnalyticsPage.js
@@ -8,30 +8,30 @@ const AnalyticsPage = () => {
 
   return (
     <>
-    <div className="flex pt-2 justify-between">
-        <h1 className="text-lg font-bold">Аналітика</h1>
-    </div>
-    <div className=" flex relative pt-2">
-        <DateRange  />
-    </div>
-    <div className="flex-grow relative pt-2 ">
-        <Map
-          markers={[
-            {
-              id: 1,
-              type: "event",
-              position: [49.84108232367849, 24.030532836914066],
-              selected: false
-            },
-            {
-              id: 2,
-              type: "rescuer",
-              position: [49.84047343621103, 24.045767784118652],
-              selected: false
-            }
-          ]} />
+      <div className="flex flex-row justify-between py-8">
+        <h1 className="text-body-900 text-lg font-bold">Аналітика</h1>
       </div>
-      {/* <LoadingOverlay /> */}
+      <div className="flex relative py-6">
+          <DateRange />
+      </div>
+      <div className="flex-grow relative pt-2 w-1/2">
+          <Map
+            markers={[
+              {
+                id: 1,
+                type: "event",
+                position: [49.84108232367849, 24.030532836914066],
+                selected: false
+              },
+              {
+                id: 2,
+                type: "rescuer",
+                position: [49.84047343621103, 24.045767784118652],
+                selected: false
+              }
+            ]} />
+      </div>
+        {/* <LoadingOverlay /> */}
     </>
     
   );
