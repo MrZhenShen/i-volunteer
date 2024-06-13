c0474834d6c2170e26cc3ade3af27f63fc054244
Ihor_Ostapovych <iostapovychweb@gmail.com>
2024-06-13 18:06:38 +0300

    feat(MapPage): add fetchVolunteer action

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,21 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Menu } from '../components/Menu';
 import Button from "../components/Button";
 import { Map } from "../components/map/Map";
 import LoadingOverlay from '../components/LoadingOverlay';
 import VolunteerInfoSlideOver from '../features/volunteers/components/VolunteerInfoSlideOver';
+import { fetchVolunteers } from "../features/volunteers/thunks";
+
+function volunteersToMarkers(volunteers) {
+  return volunteers.map((volunteer) => ({
+    id: `rescuer/${volunteer.id}`,
+    type: 'rescuer', // volunteer.role,
+    position: [49.83002537341331, 24.06477928161621],
+    selected: false,
+  }));
+}
 
 export function MapPage() {
+  const dispatch = useDispatch();
   const [selected, setSelected] = useState("all");
-  const [showDialog, setShowDialog] = useState(true)
-  const loading = false;
+  const [showDialog, setShowDialog] = useState(false)
+
+  let markers = [];
+
+  const {
+    data: volunteers,
+    loading,
+  } = useSelector((state) => state.volunteers);
+
+  useEffect(() => {
+    dispatch(fetchVolunteers({
+      page: 1,
+      size: 10,
+      sortBy: "",
+      sortOrder: "",
+      filter: "",
+    }));
+  }, [dispatch]);
 
   const filters = [
     { label: "Всі", value: "all", icon: 'Checkmark' },
     { label: "Медики", value: "medic", icon: 'Doctor' },
     { label: "Поліцейські", value: "policement", icon: 'Police' },
   ];
+
+  if (volunteers.length > 0) {
+    markers = volunteersToMarkers(volunteers);
+  }
 
   return (
     <>
@@ -30,25 +62,11 @@ export function MapPage() {
       </div>
 
       <div className="relative pt-4 flex-grow">
-        <Map
-          markers={[
-            {
-              id: 1,
-              type: "event",
-              position: [49.84108232367849, 24.030532836914066],
-              selected: false
-            },
-            {
-              id: 2,
-              type: "rescuer",
-              position: [49.84047343621103, 24.045767784118652],
-              selected: false
-            }
-          ]} />
+        <Map markers={markers} />
       </div>
       {loading && <LoadingOverlay />}
       <VolunteerInfoSlideOver
-        volunteer={{ status: 'AVAILABLE', correlationId: "2b69deb8-4795-43f1-936a-dc1ce43ae368", firstName: "John", lastName: "Doe", address: { city: "Lviv", street: "Під Голоском", zipCode: "Ukraine"}, latitude: 49.84108232367849, longitude: 24.030532836914066 }}
+        volunteer={{ status: 'AVAILABLE', correlationId: "2b69deb8-4795-43f1-936a-dc1ce43ae368", firstName: "John", lastName: "Doe", address: { city: "Lviv", street: "Під Голоском", zipCode: "Ukraine" }, latitude: 24.06477928161621, longitude: 49.83002537341331 }}
         isOpen={showDialog}
         toggle={() => setShowDialog(!showDialog)} />
     </>
